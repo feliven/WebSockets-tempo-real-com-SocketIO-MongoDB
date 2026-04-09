87e78c3688a63816cb82e6ece95acda5030157f0
FVS-MBA <feliven@gmail.com>
2026-04-09 17:47:17 -0300

    implementa SocketIO

--- src/Server.ts
+++ src/Server.ts
@@ -1,12 +1,23 @@
 import express from "express";
 import url from "url";
 import path from "path";
+import http from "http";
+import { Server } from "socket.io";
 
 const app = express();
+const httpServer = http.createServer(app);
+const io = new Server(httpServer);
+
 const port = Number(process.env.port || 3000);
 
 const pathAtual = url.fileURLToPath(import.meta.url);
 const pathPublic = path.join(pathAtual, "../../public");
 app.use(express.static(pathPublic));
 
-app.listen(port);
+io.on("connection", (socket) => {
+  console.log("um usuário se conectou");
+});
+
+httpServer.listen(port, () => {
+  console.log("Server running on port 3000");
+});
